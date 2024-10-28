dc67fae000d6e6863f7a0a0a295db9bb56f1e725
muhammadasadullah0 <muhdasadullahsiddiqui@gmail.com>
2024-10-28 16:55:32 +0500

    Update java_1.js

--- java_1.js
+++ java_1.js
@@ -1,5 +1,43 @@
-const a = "asad"
-let b = "bike"
-var c = "cat"
+let score = "true" // let is consider a multiple times 
 
-console.table([a, b, c])
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0
+
+
+let isLoggedIn = "asad" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true : 0 => false 
+// "" => false 
+// "asad" => false 
+
+let someNumber = 33 ;
+
+let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber);
+
+// let a = "ASADULLAH";
+// let b = Number (a)
+// console.log(typeof b);
+
+
+// 0
+
+let ab = .3
+let ax = Boolean (ab)
+
+console.log(ax);
