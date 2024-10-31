f3a0e3d107612368715d584c95494ae182cff33e
muhammadasadullah0 <muhdasadullahsiddiqui@gmail.com>
2024-10-31 13:31:52 +0500

    Create array_.js

---
+++ array_.js
@@ -0,0 +1,47 @@
+/* =========== array ============ 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHero = ["Iron Man" , 'Dr.Strange', 'Spider-Man']
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr[0]); // this is the way to check the value of an array
+console.log(myArr2[0]); // this is the way to check the value of an array
+console.log(myHero[0,1,2]); // this is the way to check the value of an array
+
+*/
+
+/* ========= Array Methods ========== 
+const myArr = [0, 1, 2, 3, 4, 5]
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(9)
+myArr.shift()
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof newArr);
+
+===== Differentiate between "SPLICE" & "SLICE" =====
+
+console.log("A" ,myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+
+console.log("B" ,myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log("C" ,myArr);
+console.log(myn2);
+
+*/
+
+
