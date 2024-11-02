bd0d8485b3d0f775a9d038ffcdad73c48b2c1a93
muhammadasadullah0 <muhdasadullahsiddiqui@gmail.com>
2024-11-02 13:36:53 +0500

    Create java_.js

---
+++ java_.js
@@ -0,0 +1,158 @@
+// let is consider a multiple times 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0
+
+
+let isLoggedIn = "asad" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true : 0 => false 
+// "" => false 
+// "asad" => false 
+
+let someNumber = 33 ;
+
+let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber);
+
+// let a = "ASADULLAH";
+// let b = Number (a)
+// console.log(typeof b);
+
+
+// 0
+
+// let ab = .3
+// let ax = Boolean (ab)
+
+// console.log(ax);
+
+
+
+// const isLOGGEDINTHREETIMES = "ASADULLAH SIDDIQUI"
+// const isLOGGEDINTWOTIMES = Boolean (isLOGGEDINTHREETIMES)
+
+// console.log(isLOGGEDINTWOTIMES);
+ 
+
+/*
+====((primitive)=>Stack) & ((Non-Primitive)=>Hype))====
+let myChannelName = "ASADULLAH"
+let anothername = myChannelName
+anothername = "Chai WITH ME @@@"
+
+console.log(anothername);
+console.log(myChannelName);
+
+let userOne = {
+    email : "user@abc.com",
+    upi : "user@abc.com"
+}
+let userTwo = userOne
+userTwo.email = "abc@jit.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+*/
+
+
+
+/*
+========== String Methods ===========
+const name = "ASAD "
+const repoCount = 50 
+console.log(`Hello my name is ${name}and my repo Count is ${repoCount}`);
+
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("d"));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const newStringOne =  "  Asadullah  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https//google.com/asadullah*siddiqui"
+console.log(url.replace( '*' , "-"))
+
+const url = "https//google.com/asadullahsiddiqui"
+console.log(url.includes('asadullah'));
+
+const gameName = new String('Asadullah-name-is')
+console.log(gameName.split('-'));
+*/
+
+/*
+// =========== Numbers METHODS ============
+const score = 200 
+console.log(score);
+
+const balance = new Number(200)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.456
+console.log(otherNumber.toPrecision(6));
+
+const hundreds = 100000;
+console.log(hundreds.toLocaleString('PKR'));
+
+*/
+
+/*
+============== MATHS ============== 
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(3.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+console.log(Math.random()*10 + 1);
+*/
+
+
+/* 
+============= Dates ============== 
+
+let newDate = new Date()
+console.log(newDate.getDate());
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth());
+console.log(newDate.getMinutes());
+console.log(newDate.getUTCHours());
+console.log(newDate.getTime());
+console.log(newDate.toLocaleDateString());
+console.log(newDate.toLocaleTimeString());
+console.log(newDate.toISOString());
+console.log(newDate.toJSON());
+
+console.log(newDate.toLocaleString('default',{
+    weekday: "long",
+    day :"2-digit",
+}))
+  
+
+*/
+
